8c5f87264cf7d161ed65dc79cf2f0da927758928
Stoyansl <Stoyansl29@gmail.com>
2018-01-31 06:10:20 +0200

    Upload Exercises

---
+++ Streams/StreamsExercises/OddLines/OddLines.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class OddLines
+{
+    static void Main()
+    {
+        using (var reader = new StreamReader("text.txt"))
+        {
+            int numberLine = 0;
+            while (true)
+            {
+                string line =reader.ReadLine();
+                if (line==null)
+                {
+                    break;
+                }
+                if (numberLine%2!=0)
+                {
+                    Console.WriteLine(line);
+                }
+                numberLine++;
+            }
+        }
+    }
+}
+
